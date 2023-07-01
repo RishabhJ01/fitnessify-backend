44946058df59a74c1959616c7ae4104ae6c2ddbe
Siddharth Akar <siddharth.akar@qoala.id>
2023-07-01 14:45:02 +0530

    feat: :sparkles: added chat gpt apis
    added chat gpt apis

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+const mongoose=require('mongoose');
+const axios = require('axios');
+const gptRoutes=require('./routes/chatgpt.js');
 const app = express();
-const port = 8080;
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.use(express.json());
+app.use("/gpt",gptRoutes)
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+
+
+
