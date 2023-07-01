3e6af61ee172e96d995a89960b17b4e76f1a085e
Siddharth Akar <siddharth.akar@qoala.id>
2023-07-01 16:00:02 +0530

    feat: :sparkles: upated chat gpt apis

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
+const dotenv=require("dotenv");
+dotenv.config();
 import express from 'express';
 const mongoose=require('mongoose');
 const axios = require('axios');
-const dotenv=require("dotenv");
+
 const gptRoutes=require('./routes/chatgpt.js');
 const app = express();
 const port = 3000;
 
-dotenv.config();
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- src/routes/chatgpt.ts
+++ src/routes/chatgpt.ts
@@ -1,113 +1,132 @@
 const router=require("express").Router();
 const axios = require('axios');
+const { Configuration, OpenAIApi } = require("openai");
 
+console.log(process.env.OPEN_SOURCE_API_TOKEN)
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_SOURCE_API_TOKEN,
+});
+const openai = new OpenAIApi(configuration);
+async function runCompletion (prompt) {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+  });
+  return completion.data.choices[0].text
+  // console.log(completion.data.choices[0].text);
+}
+  
+router.get('/response',async (req, res) => {
+    var resp=runCompletion(req.message);
+    res.status(200).json({resp });
+ })
+// const token=process.env.OPEN_SOURCE_API_TOKEN
 
-const token=process.env.OPEN_SOURCE_API_TOKEN
-// API endpoint for creating a chat session
-router.post('/chat/session', async (req, res) => {
-    try {
-      const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-        model: 'gpt-3.5-turbo',
-        messages: [{ role: 'system', content: 'You are a fitness and wellness assistant.' }],
-      }, {
-        headers: {
-            'Authorization': 'Bearer '+token,
-          'Content-Type': 'application/json',
-        },
-      });
+// // API endpoint for creating a chat session
+// router.post('/chat/session', async (req, res) => {
+//     try {
+//       const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+//         model: 'gpt-3.5-turbo',
+//         messages: [{ role: 'system', content: 'You are a fitness and wellness assistant.' }],
+//       }, {
+//         headers: {
+//             'Authorization': 'Bearer '+token,
+//           'Content-Type': 'application/json',
+//         },
+//       });
   
-      res.json({ sessionId: response.data.id });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Failed to create chat session' });
-    }
-  });
+//       res.json({ sessionId: response.data.id });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Failed to create chat session' });
+//     }
+//   });
   
-  // API endpoint for sending user messages
-router.post('/chat/message', async (req, res) => {
-    const { sessionId, message } = req.body;
+//   // API endpoint for sending user messages
+// router.post('/chat/message', async (req, res) => {
+//     const { sessionId, message } = req.body;
   
-    try {
-      const response = await axios.post(`https://api.openai.com/v1/chat/completions/${sessionId}/messages`, {
-        messages: [{ role: 'system', content: message }],
-      }, {
-        headers: {
-          'Authorization':`Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      });
+//     try {
+//       const response = await axios.post(`https://api.openai.com/v1/chat/completions/${sessionId}/messages`, {
+//         messages: [{ role: 'system', content: message }],
+//       }, {
+//         headers: {
+//           'Authorization':`Bearer ${token}`,
+//           'Content-Type': 'application/json',
+//         },
+//       });
   
-      res.json({ reply: response.data.choices[0].message.content });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Failed to send user message' });
-    }
-  });
+//       res.json({ reply: response.data.choices[0].message.content });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Failed to send user message' });
+//     }
+//   });
   
-  // API endpoint for retrieving chat history
-router.get('/chat/history/:sessionId', async (req, res) => {
-    const sessionId = req.params.sessionId;
+//   // API endpoint for retrieving chat history
+// router.get('/chat/history/:sessionId', async (req, res) => {
+//     const sessionId = req.params.sessionId;
   
-    try {
-      const response = await axios.get(`https://api.openai.com/v1/chat/completions/${sessionId}`, {
-        headers: {
-          'Authorization': `Bearer ${token}`
-        },
-      });
+//     try {
+//       const response = await axios.get(`https://api.openai.com/v1/chat/completions/${sessionId}`, {
+//         headers: {
+//           'Authorization': `Bearer ${token}`
+//         },
+//       });
   
-      res.json({ history: response.data.messages });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Failed to retrieve chat history' });
-    }
-  });
+//       res.json({ history: response.data.messages });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Failed to retrieve chat history' });
+//     }
+//   });
   
-  // API endpoint for generating suggested responses
-  router.post('/chat/suggestions', async (req, res) => {
-    const { sessionId } = req.body;
+//   // API endpoint for generating suggested responses
+//   router.post('/chat/suggestions', async (req, res) => {
+//     const { sessionId } = req.body;
   
-    try {
-      const response = await axios.post(`https://api.openai.com/v1/chat/completions/${sessionId}/messages`, {
-        messages: [{ role: 'system', content: 'Suggest' }],
-      }, {
-        headers: {
-            'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      });
+//     try {
+//       const response = await axios.post(`https://api.openai.com/v1/chat/completions/${sessionId}/messages`, {
+//         messages: [{ role: 'system', content: 'Suggest' }],
+//       }, {
+//         headers: {
+//             'Authorization': `Bearer ${token}`,
+//           'Content-Type': 'application/json',
+//         },
+//       });
   
-      const suggestions = response.data.choices.map(choice => choice.message.content);
-      res.json({ suggestions });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Failed to generate suggestions' });
-    }
-  });
+//       const suggestions = response.data.choices.map(choice => choice.message.content);
+//       res.json({ suggestions });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Failed to generate suggestions' });
+//     }
+//   });
   
-  router.get("/response",(req,res)=>{
-    // const {message} =req.body
-    var message='hello how are you ?'
-    var resp=generateChatMessage(message)
-    res.status(200).json({resp})
-  })
+//   router.get("/response",(req,res)=>{
+//     // const {message} =req.body
+//     var message='hello how are you ?'
+//     var resp=generateChatMessage(message)
+//     res.status(200).json({resp})
+//   })
 
-  async function generateChatMessage(prompt) {
-    try {
-      const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-        model: 'gpt-3.5-turbo',
-        messages: [{ role: 'system', content: 'You are a fitness and wellness assistant.' }, { role: 'user', content: prompt }],
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-      });
+//   async function generateChatMessage(prompt) {
+//     try {
+//       const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+//         model: 'gpt-3.5-turbo',
+//         messages: [{ role: 'system', content: 'You are a fitness and wellness assistant.' }, { role: 'user', content: prompt }],
+//       }, {
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'Authorization': `Bearer ${token}`,
+//         },
+//       });
   
-      return response.data.choices[0].message.content;
-    } catch (error) {
-      console.error('Failed to generate chat message:', error);
-      throw error;
-    }
-  }
+//       return response.data.choices[0].message.content;
+//     } catch (error) {
+//       console.error('Failed to generate chat message:', error);
+//       throw error;
+//     }
+//   }
 
   module.exports=router
